e25b926eec490f736dec99d90d7807993d3b58ee
ZerySue <q.ynan@163.com>
2016-09-26 16:04:31 +0800

    增加基于泛型的抽象工厂

---
+++ DALFactory/DataAccess.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALFactory
+{
+    /// <summary>
+    /// 抽象工厂模式创建DAL-(利用工厂模式+泛型机制+反射机制+缓存机制,实现动态创建不同的数据层对象接口) 。
+    /// 可以在这个DataAccess类里创建所有DAL类
+    /// </summary>
+    public sealed class DataAccess
+    {
+        private static readonly string path = "MyFrame";//ConfigurationManager.AppSettings["DAL"];
+        /// <summary>
+        /// 采用泛型创建对象或从缓存中获取
+        /// </summary>
+        /// <typeparam name="T">数据类型</typeparam>
+        /// <returns></returns>
+        public static T CreateObject<T>()
+        {
+            //此处采用XML，把对应关系做好更灵活
+            string typeName = "." + typeof(T).Name.Substring(1) + "DAL";
+            object obj;
+            //从缓存中读取数据
+            obj = DataCache.GetCache(typeName);
+            if (obj == null)
+            {
+                //反射创建
+                obj = Assembly.Load(path).CreateInstance(path + typeName);
+                DataCache.SetCache(typeName, obj);
+            }
+            return (T)obj;
+        }
+    }
+}
--- frame0/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="frame0.MvcApplication" Language="C#" %>
