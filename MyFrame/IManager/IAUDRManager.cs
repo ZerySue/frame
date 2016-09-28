724a22152b71c9615654a3042dc56e4a4a85af5e
ZerySue <q.ynan@163.com>
2016-09-28 16:08:54 +0800

    增加样式

--- MyFrame/IManager/IAUDRManager.cs
+++ MyFrame/IManager/IAUDRManager.cs
@@ -1,19 +1,19 @@
-﻿using MyFrame.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
 
-namespace MyFrame
-{ 
-    /// <summary>
-    /// 增删改查的统一接口
-    /// </summary>
+namespace MyFrame.Manager
+{
     public interface IAUDRManager
-    {
+    {    /// <summary>
+        /// 增删改查的统一接口
+        /// </summary>
+
         List<object> GetDaga(object dataInfo);
         bool AddData(object logInfo);
         bool UpdateData(object dataInfo);
         bool DeleteData(object dataInfo);
+
     }
 }
